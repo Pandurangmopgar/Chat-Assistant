a7718aa34e885676df887ae7f3d57a97b6feebca
Pandurangmopgar <90682480+Pandurangmopgar@users.noreply.github.com>
2024-09-05 21:05:09 +0530

    files been added

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Sidebar from './components/Sidebar/Sidebar'
 import Mainn from './components/Main/Mainn'
-// import Main from './components/Main/Main'
-// import './components/Sidebar.css'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-
- <>
- <Sidebar/>
- <Mainn/>
- </>
- )
+    <>
+      <Sidebar />
+      <Mainn />
+      <SpeedInsights />
+    </>
+  )
 }
 
 export default App
